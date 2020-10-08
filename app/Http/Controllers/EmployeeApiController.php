cd6f8eeb2ff107bb8d0d2dc856626428433cf9ec
Umberto <umbertopellegrino96@gmail.com>
2020-10-08 16:01:12 +0200

    2 rotte

---
+++ app/Http/Controllers/EmployeeApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Employee;
+
+
+class EmployeeApiController extends Controller{
+  public function all(){
+    $emps = Employee::all()
+
+    return response() -> json($emps);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'EmployeeController@index') -> name('emp-index');
+Route::get('/api/employee', 'EmployeeApiController@all') -> name('emp-api-index');
